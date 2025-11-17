8d58d6783ae667943533d48d74ab08d82688bdcc
Dengtiel <d.akol@alustudent.com>
2025-11-17 11:58:59 +0200

    Initial commit for Riseup Africa project

---
+++ apps/web/components/providers.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { Toaster } from "@workspace/ui/components/sonner"
+import { AuthProvider } from "@/hooks/use-auth"
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+    >
+      <AuthProvider>
+        {children}
+        <Toaster />
+      </AuthProvider>
+    </NextThemesProvider>
+  )
+}
